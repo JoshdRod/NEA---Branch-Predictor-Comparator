f22c1658bc116b3896e7b0a703796430bbe8af2c
Joshua Rodriguez <joshrod1089@gmail.com>
2025-01-26 17:22:18 +0000

    Refactored project folders

---
+++ main.py
@@ -0,0 +1,8 @@
+from CPU.Processor import Processor
+
+P = Processor()
+
+# Allow user to select algorithm
+
+# Allow user to select branch predictors to run
+# Display graph of mispredictions/cycles
