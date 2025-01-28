b76b4a69d590af928a14930a8b2a5423d728b592
Joshua Rodriguez <joshrod1089@gmail.com>
2025-01-28 09:34:02 +0000

    Decomposed basepredictor - need to finish comments on abstract methods

--- CPU/DirectionPredictors.py
+++ CPU/DirectionPredictors.py
@@ -1,7 +1,28 @@
 class BasePredictor:
-    
-    def __init__(self):
-        self.name = "Always Not Taken"
+    def __init__(self, name):
+        self.name = name
+
+    """
+    Predicts the next instruction to fetch based on the program counter
+    INPUT: int program counter
+    RETURNS: int predicted next program counter
+    """
+    def Predict(self, programCounter: int) -> int:
+        pass
+
+    """
+    Updates branch target buffer with the given branch instruction
+    INPUT:
+    """
+    def Update(self):
+        pass
+
+    def Stall(self):
+        pass
+
+class AlwaysNotTaken(BasePredictor): 
+    def __init__(self, name):
+        super.__init__(name)
 
     stalled = True # Predictor stalls after an unsuccessful branch (e.g: branch to 10, fetch 10, then predict 11..)
     def Predict(self, programCounter: int):
